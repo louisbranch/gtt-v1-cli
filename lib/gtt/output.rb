19caf2e5be68f3c54d0bbf21ccf8b4fc2adb3713
Luiz Branco <me@luizbranco.com>
2012-09-25 21:05:31 -0300

    Improve error handler

--- lib/gtt/output.rb
+++ lib/gtt/output.rb
@@ -5,11 +5,29 @@ module Gtt
 
     def initialize(response)
       if response['ok']
-        puts 'OK!'.color(:green)
+        output = render_success(response)
+      elsif response['error']
+        output = render_error(response)
       else
-        error = "ERROR:".color(:red)
-        puts "#{error} #{response['message']}"
+        output = 'Unknown message'.color(:yellow)
       end
+      puts output
+    end
+
+    def render_success(response)
+      output = 'OK! '.color(:green)
+      if response['message']
+        output += response['message'].to_s
+      end
+      output
+    end
+
+    def render_error(response)
+      output = "ERROR: ".color(:red)
+      if response['message']
+        output += response['message'].to_s
+      end
+      output
     end
 
   end
